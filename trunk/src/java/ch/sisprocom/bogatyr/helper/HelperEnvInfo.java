f070f82fd0c9843bc37379853f1dc334ef9fa631
laubenberger <laubenberger@a2a31221-ce33-0410-a744-516c3ff6c6e9>
2009-04-29 23:14:25 +0000

    -HelperEnvInfo extended with some new methods -HelperGeneral.getFS() added

--- trunk/src/java/ch/sisprocom/bogatyr/helper/HelperEnvInfo.java
+++ trunk/src/java/ch/sisprocom/bogatyr/helper/HelperEnvInfo.java
@@ -32,8 +32,6 @@
 package ch.sisprocom.bogatyr.helper;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Map;
 
 
@@ -43,7 +41,7 @@ import java.util.Map;
  * It also provides informations about vm memory, temp/user directory and variables.
  * 
  * @author Stefan Laubenberger
- * @version 20090429
+ * @version 20090430
  */
 public abstract class HelperEnvInfo {
 	/**
@@ -72,6 +70,33 @@ public abstract class HelperEnvInfo {
 	public static long getMemoryMax() {
 		return Runtime.getRuntime().maxMemory();
 	}
+
+	/**
+	 * Returns the current Java runtime version.
+     *
+     * @return current Java runtime version
+	 */
+	public static String getJavaVersion() {
+		return System.getProperties().getProperty("java.version"); //$NON-NLS-1$
+	}
+	
+	/**
+	 * Returns the current class path.
+     *
+     * @return current class path
+	 */
+	public static String getClassPath() {
+		return System.getProperties().getProperty("java.class.path"); //$NON-NLS-1$
+	}
+	
+	/**
+	 * Returns the current class path.
+     *
+     * @return current class path
+	 */
+	public static String getLibraryPath() {
+		return System.getProperties().getProperty("java.library.path"); //$NON-NLS-1$
+	}
 	
 	/**
 	 * Returns all available processors for the VM.
@@ -87,7 +112,7 @@ public abstract class HelperEnvInfo {
      *
      * @return OS architecture
 	 */
-	public static Object getOsArch() {
+	public static String getOsArch() {
 		return System.getProperties().getProperty("os.arch"); //$NON-NLS-1$
 	}
 
@@ -112,16 +137,27 @@ public abstract class HelperEnvInfo {
 	/**
 	 * Returns all system environment variables.
 	 *
-	 * @return list of system environment variables
+	 * @return map of system environment variables
 	 */
-	public static Object getOsEnvironmentVariables() {
-		final Map<String, String> map = System.getenv();
-		final Collection<String> list = new ArrayList<String>(map.size());
+	public static Map<String, String> getOsEnvironmentVariables() {
+		return System.getenv();
+//		final Map<String, String> map = System.getenv();
+//		final Collection<String> list = new ArrayList<String>(map.size());
+//
+//		for (final Map.Entry<String, String> pair : map.entrySet()) {
+//            list.add(pair.getKey() + '=' + pair.getValue());
+//        }
+//        return list;
+    }
 
-		for (final Map.Entry<String, String> pair : map.entrySet()) {
-            list.add(pair.getKey() + '=' + pair.getValue());
-        }
-        return list;
+	/**
+	 * Returns the value of a system environment variable.
+	 *
+	 * @param variable of the environment (e.g. PATH)
+	 * @return value of the system environment variable
+	 */
+	public static String getOsEnvironmentVariable(String variable) {
+		return System.getenv(variable);
     }
 	
 	/**
@@ -134,21 +170,30 @@ public abstract class HelperEnvInfo {
 	}
 	
 	/**
-	 * Returns the user directory of the current user.
+	 * Returns the user home directory of the current user.
      *
      * @return user home directory
 	 */
-	public static Object getUserHomeDirectory() {
+	public static File getUserHomeDirectory() {
 		return new File(System.getProperty("user.home")); //$NON-NLS-1$
 	}
 	
 	/**
-	* Returns the directory where the application has been started.
+	* Returns the current user directory.
 	*
-	* @return application home directory
+	* @return current user directory
 	*/
-	public static File getWorkingDirectory() {
+	public static File getUserDirectory() {
 		return new File(System.getProperty("user.dir")); //$NON-NLS-1$
+	}
+
+	/**
+	* Returns the name of the current user.
+	*
+	* @return name of the current user
+	*/
+	public static String getUserName() {
+		return System.getProperty("user.name"); //$NON-NLS-1$
 	}
 	
 	/**
