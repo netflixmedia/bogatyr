7e52da66fcac5f246571d05a4a104245ed04ab92
laubenberger <laubenberger@a2a31221-ce33-0410-a744-516c3ff6c6e9>
2010-07-14 19:13:30 +0000

    -AdapterDimension added

--- src/main/java/net/laubenberger/bogatyr/misc/Constants.java
+++ src/main/java/net/laubenberger/bogatyr/misc/Constants.java
@@ -59,7 +59,7 @@ public abstract class Constants {
 	private static final Logger log = LoggerFactory.getLogger(Constants.class);
 
 	//Bogatyr specific
-	public static final Document BOGATYR = new DocumentImpl("Bogatyr", new BigDecimal("0.93"), 282, HelperTime.getDate(2010, 7, 14, 17, 31, 0), UUID.fromString("4d8f7b88-2a1e-4f74-98ca-99d8a0cf97a5"), null, null, null, null); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	public static final Document BOGATYR = new DocumentImpl("Bogatyr", new BigDecimal("0.93"), 283, HelperTime.getDate(2010, 7, 14, 21, 12, 0), UUID.fromString("4d8f7b88-2a1e-4f74-98ca-99d8a0cf97a5"), null, null, null, null); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 	//defaults
 //	public static final MathContext DEFAULT_MATHCONTEXT = MathContext.DECIMAL128;
---
+++ src/main/java/net/laubenberger/bogatyr/misc/xml/adapter/AdapterDimension.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2007-2010 by Stefan Laubenberger.
+ *
+ * Bogatyr is free software: you can redistribute it and/or modify
+ * it under the terms of the General Public License v2.0.
+ *
+ * Bogatyr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details:
+ * <http://www.gnu.org/licenses>
+ *
+ * This distribution is available at:
+ * <http://code.google.com/p/bogatyr/>
+ * <http://dev.laubenberger.net/bogatyr/>
+ *
+ * Contact information:
+ * Stefan Laubenberger
+ * Bullingerstrasse 53
+ * CH-8004 Zuerich
+ *
+ * <http://www.laubenberger.net>
+ *
+ * <laubenberger@gmail.com>
+ */
+
+package net.laubenberger.bogatyr.misc.xml.adapter;
+
+import java.awt.Dimension;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import net.laubenberger.bogatyr.misc.xml.XmlEntry;
+import net.laubenberger.bogatyr.misc.xml.XmlMap;
+
+/**
+ * Map adapter for {@link Dimension}.
+ *
+ * @author Stefan Laubenberger
+ * @version 0.9.3 (20100714)
+ * @since 0.9.3
+ */
+public class AdapterDimension extends XmlAdapter<XmlMap, Dimension> {
+
+	/*
+	 * Overridden methods
+	 */
+
+	@Override
+	public XmlMap marshal(final Dimension dim) throws Exception {
+		if (null != dim) {
+			final XmlMap xmlMap = new XmlMap();
+
+			xmlMap.getEntries().add(new XmlEntry("x", String.valueOf(dim.width)));
+			xmlMap.getEntries().add(new XmlEntry("y", String.valueOf(dim.height)));
+
+			return xmlMap;
+		}
+		return null;
+	}
+
+	@Override
+	public Dimension unmarshal(final XmlMap xmlMap) throws Exception {
+		if (null != xmlMap && xmlMap.getEntries().size() == 2) {
+			
+			return new Dimension(Integer.valueOf(xmlMap.getEntries().get(0).getValue()), Integer.valueOf(xmlMap.getEntries().get(1).getValue()));
+		}
+		return null;
+	}
+}
